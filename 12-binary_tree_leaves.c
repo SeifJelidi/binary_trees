a208b53c049ef793e592ba425113e38c02901dbf
Seif Jelidi <seifjelidi3@gmail.com>
2020-07-24 18:41:56 +0000

    counting leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,7 +13,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
+	if (!tree->left && !tree->right)
 		return (1);
 
 	s = binary_tree_leaves(tree->right) + binary_tree_leaves(tree->left);
